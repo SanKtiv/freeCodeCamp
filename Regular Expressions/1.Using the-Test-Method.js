ae633e67ae95f04722a9e75bcf92e9922ec40a56
SanKtiv <aktitorov@gmail.com>
2023-09-11 08:40:52 +0300

    add 1.Using the-Test-Method.js

---
+++ Regular Expressions/1.Using the-Test-Method.js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+//Apply the regex myRegex on the string myString using the .test() method.
+//You should use .test() to test the regex.
+// Waiting:Your result should return true.
+
+//SOLUTION:
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
