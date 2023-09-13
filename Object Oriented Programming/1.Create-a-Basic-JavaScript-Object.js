bfea9f3875624ed869b85bcd92c743335ae52eca
SanKtiv <aktitorov@gmail.com>
2023-09-13 10:41:06 +0300

    add 1.Create-a-Basic-JavaScript-Object.js

---
+++ Object Oriented Programming/1.Create-a-Basic-JavaScript-Object.js
@@ -0,0 +1,11 @@
+//DESCRIPTION:
+//Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+//dog should be an object.
+// Passed:dog should have a name property set to a string.
+// Passed:dog should have a numLegs property set to a number.
+
+//SOLUTION:
+let dog = {
+    name: 'Reks',
+    numLegs: 4
+};
