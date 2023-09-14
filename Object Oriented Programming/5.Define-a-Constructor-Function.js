33c716534f4cce8fc71c5b843e1c2cc2568a1c43
SanKtiv <aktitorov@gmail.com>
2023-09-14 09:15:42 +0300

    add 5.Define-a-Constructor-Function.js

---
+++ Object Oriented Programming/5.Define-a-Constructor-Function.js
@@ -0,0 +1,12 @@
+//DESCRIPTION:
+//Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+//Dog should have a name property set to a string.
+// Waiting:Dog should have a color property set to a string.
+// Waiting:Dog should have a numLegs property set to a number.
+
+//SOLUTION:
+function Dog() {
+    this.name = 'Reks';
+    this.color = 'Blakc';
+    this.numLegs = 4;
+}
