e92bb6254267c41e6cd1154932a6d5087744db52
SanKtiv <aktitorov@gmail.com>
2023-10-02 10:25:06 +0300

    add 25.Understand-the-Immediately-Invoked-Function-Expression-(IIFE).js

---
+++ Object Oriented Programming/25.Understand-the-Immediately-Invoked-Function-Expression-(IIFE).js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+//Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function expression (IIFE).
+//The function should be anonymous.
+// Passed:Your function should have parentheses at the end of the expression to call it immediately.
+
+//SOLUTION:
+(function () {
+    console.log("A cozy nest is ready");
+})();
